1b93db315ed60b25f489bdbd50be28bc75344791
Benjamin Huang <aloneplayer@gmail.com>
2012-03-08 14:58:08 +0800

    Add project CspTest

---
+++ CspTest/CspTest/InheritanceTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace BenCspTest
+{
+    public interface IWhoAmI
+    {
+        void InterfaceWhoAmI();
+    }
+    public class BaseClass : IWhoAmI
+    {
+        public BaseClass()
+        {
+        }
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("WhoAmI? I'm BaseClass");
+        }
+
+        public virtual void VirtualWhoAmI()
+        {
+            Console.WriteLine("WhoAmI? I'm BaseClass");
+        }
+
+        public void InterfaceWhoAmI()
+        {
+            Console.WriteLine("InterfaceWhoAmI? I'm BaseClass");
+        }
+    }
+
+    public class DerivedClass : BaseClass, IWhoAmI
+    {
+        // [1] Compare the IL code of the "WhoAmI" and "new WhoAmI"
+        //
+        public new void WhoAmI()
+        {
+            Console.WriteLine("WhoAmI? I'm DerivedClass");
+        }
+
+        //public void WhoAmI()
+        //{
+        //    Console.WriteLine("WhoAmI? I'm DerivedClass");
+        //}
+
+        public override void VirtualWhoAmI()
+        {
+            Console.WriteLine("VirtualWhoAmI? I'm DerivedClass");
+        }
+
+        /// <summary>
+        /// Does not hide BaseClass.InterfaceWhoAmI
+        /// </summary>
+        void IWhoAmI.InterfaceWhoAmI()
+        {
+            Console.WriteLine("InterfaceWhoAmI? I'm DerivedClass");
+        }
+    }
+
+    public static class InheritanceTester
+    {
+        public static void TestCase1()
+        {
+            //[1]
+            BaseClass obj = new DerivedClass();
+            obj.WhoAmI();
+            obj.VirtualWhoAmI();
+
+            obj.InterfaceWhoAmI();
+            ((IWhoAmI)obj).InterfaceWhoAmI();
+        }
+    }
+}
---
+++ CspTest/CspTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BenCspTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InheritanceTester.TestCase1();
+            
+            Console.ReadLine();
+        }
+    }
+}
