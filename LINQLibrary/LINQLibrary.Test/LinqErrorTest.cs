3bea6c07923482a3958c5681c338286adb999c5f
Benjamin <Aloneplayer@gmail.com>
2012-04-02 23:45:55 +0800

    Add test project

---
+++ LINQLibrary/LINQLibrary.Test/LinqErrorTest.cs
@@ -0,0 +1,81 @@
+﻿using LINQLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace LINQLibrary.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for LinqErrorTest and is intended
+    ///to contain all LinqErrorTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class LinqErrorTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for GetSomeProduct
+        ///</summary>
+        [TestMethod()]
+        public void GetSomeProductTest()
+        {
+            LinqError target = new LinqError(); // TODO: Initialize to an appropriate value
+            //List<Product> expected = null; // TODO: Initialize to an appropriate value
+            List<Product> actual;
+            actual = target.GetSomeProduct();
+            Assert.AreEqual(2, actual.Count);
+        }
+    }
+}
