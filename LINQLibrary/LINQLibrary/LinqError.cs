9a84111dc881cb8225b182385b010dd0fe001296
Benjamin <Aloneplayer@gmail.com>
2012-04-02 23:44:39 +0800

    add new file

---
+++ LINQLibrary/LINQLibrary/LinqError.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQLibrary
+{
+    public class LinqError
+    {
+        private List<Product> products = new List<Product>() { 
+                    new Product { Name = "Gun", Count = 10, Price = 10 },
+                    new Product { Name = "Cup", Count = 5, Price = 7.5 },
+                    new Product { Name = "Pen", Count = 3, Price = 5.5 } 
+                };
+
+        public List<Product> GetSomeProduct()
+        {
+            var result = products.Where(T => T.Price > 6);
+            return result.ToList();
+        }
+
+        public List<Product> GetSomeProduct_Error()
+        {
+            var result = products.Where(T =>
+            {
+                int a = 6;
+                return T.Price > a;
+            });
+            return result.ToList();
+        }
+
+        ////return this.QueryAll().Where(item =>
+        ////    {
+        ////        var currentStatus = MerchantStatus.New;
+        ////        if (item.StatusEffectiveDate == null || item.StatusEffectiveDate.Value <= DateTime.Now)
+        ////        {
+        ////            currentStatus = item.NextStatus;
+        ////        }
+        ////        else
+        ////        {
+        ////            currentStatus = item.LastStatus;
+        ////        }
+        ////        if (currentStatus == MerchantStatus.Suspended || currentStatus == MerchantStatus.Retired)
+        ////        {
+        ////            return item;
+        ////        }
+        ////    });
+    }
+}
