14af2cf097192fae547eae4fa31d17da7f1471fd
Benjamin Huang <aloneplayer@gmail.com>
2012-12-04 17:52:59 -0500

    WCF Demo

---
+++ WCFDemo/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Contracts;
+using System.ServiceModel;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ChannelFactory<ICalculator> channelFactory = new ChannelFactory<ICalculator>(new WSHttpBinding(), "http://127.0.0.1:9999/calculatorservice"))
+            {
+                ICalculator proxy = channelFactory.CreateChannel();
+                using (proxy as IDisposable)
+                {
+                    Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
+                    Console.WriteLine("x - y = {2} when x = {0} and y = {1}", 1, 2, proxy.Subtract(1, 2));
+                    Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
+                    Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
+                }
+            }
+        }
+    }
+}
