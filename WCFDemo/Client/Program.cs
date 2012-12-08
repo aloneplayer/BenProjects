54cd1561a30434806b107857a0b039dde5d8f7de
Benjamin Huang <aloneplayer@gmail.com>
2012-12-08 08:37:08 -0500

    Add IISHost

--- WCFDemo/Client/Program.cs
+++ WCFDemo/Client/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Contracts;
 using System.ServiceModel;
+using Client.ServiceReference1;
 
 namespace Client
 {
@@ -21,6 +22,17 @@ namespace Client
                     Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
                     Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
                 }
+            }
+        }
+
+        static void MainUsingProxy()
+        {
+            using (CalculatorServiceClient proxy = new CalculatorServiceClient())
+            {
+                Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
+                Console.WriteLine("x - y = {2} when x = {0} and y = {1}", 1, 2, proxy.Subtract(1, 2));
+                Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
+                Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
             }
         }
     }
--- WCFDemo/Hosting/Program.cs
+++ WCFDemo/Hosting/Program.cs
@@ -10,8 +10,10 @@ namespace Hosting
     {
         static void Main(string[] args)
         {
+            //Create host for type CalculatorService
             using (ServiceHost host = new ServiceHost(typeof(CalculatorService)))
             {
+                //Add endpoint, which contains service contrace, biding and address
                 host.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "http://127.0.0.1:9999/calculatorservice");
                 if (host.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
                 {
@@ -20,6 +22,21 @@ namespace Hosting
                     behavior.HttpGetUrl = new Uri("http://127.0.0.1:9999/calculatorservice/metadata");
                     host.Description.Behaviors.Add(behavior);
                 }
+
+                host.Opened += delegate
+                {
+                    Console.WriteLine("CalculaorService已经启动，按任意键终止服务！");
+                };
+
+                host.Open();
+                Console.Read();
+            }
+        }
+
+        static private void MainUsingConfig()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(CalculatorService)))
+            {
                 host.Opened += delegate
                 {
                     Console.WriteLine("CalculaorService已经启动，按任意键终止服务！");
